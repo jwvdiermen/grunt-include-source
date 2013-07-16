298703f50b76c0072a759c621234c82822a03cc0
Jan Willem van Diermen <jwvdiermen@gmail.com>
2013-07-16 22:20:59 +0200

    Initial commit, pre-alpha version.

---
+++ README.md
@@ -0,0 +1,89 @@
+# grunt-include-source
+
+> Include your sources into your HTML files automatically. Also supports including Bower components that expose their source files via the *sources* property in the `bower.json` file.
+
+## Getting Started
+This plugin requires Grunt `~0.4.1`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-include-source --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-include-source');
+```
+
+## The "include_source" task
+
+### Overview
+In your project's Gruntfile, add a section named `include_source` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  include_source: {
+    options: {
+      // Task-specific options go here.
+    },
+    your_target: {
+      // Target-specific file lists and/or options go here.
+    },
+  },
+})
+```
+
+### Options
+
+#### options.separator
+Type: `String`
+Default value: `',  '`
+
+A string value that is used to do something with whatever.
+
+#### options.punctuation
+Type: `String`
+Default value: `'.'`
+
+A string value that is used to do something else with whatever else.
+
+### Usage Examples
+
+#### Default Options
+In this example, the default options are used to do something with whatever. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result would be `Testing, 1 2 3.`
+
+```js
+grunt.initConfig({
+  include_source: {
+    options: {},
+    files: {
+      'dest/default_options': ['src/testing', 'src/123'],
+    },
+  },
+})
+```
+
+#### Custom Options
+In this example, custom options are used to do something else with whatever else. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result in this case would be `Testing: 1 2 3 !!!`
+
+```js
+grunt.initConfig({
+  include_source: {
+    options: {
+      separator: ': ',
+      punctuation: ' !!!',
+    },
+    files: {
+      'dest/default_options': ['src/testing', 'src/123'],
+    },
+  },
+})
+```
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
---
+++ tasks/include_source.js
@@ -0,0 +1,189 @@
+/*
+ * grunt-include-source
+ * https://github.com/jwvdiermen/grunt-include-source
+ *
+ * Copyright (c) 2013 Jan Willem van Diermen
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+	var path = require('path');
+	var os = require('os');
+	var util = require('util');
+
+	// Parses input HTML and returns an array of include statements and their position.
+	var parseHtml = function(source) {
+		var re = /<!--\s+include:\s+(.*)\s+-->/gi,
+			matches,
+			results = [];
+
+		grunt.log.debug('Parsing HTML...');
+		while ((matches = re.exec(source)) !== null) {
+			if (matches.length <= 1) continue;
+			grunt.log.debug('Got match, options are { ' + matches[1] + ' }.');
+			var optionsObj = JSON.parse("{" + matches[1] + "}");
+			results.push({
+				start: matches.index,
+				end: matches.index + matches[0].length - 1,
+				options: optionsObj
+			});
+		}
+
+		return results;
+	};
+
+	// Parses input SASS.
+	var parseSass = function(source) {
+		var re = /\/\/\s+include:\s+(.*)/gi,
+			matches,
+			results = [];
+
+		grunt.log.debug('Parsing SASS...');
+		while ((matches = re.exec(source)) !== null) {
+			if (matches.length <= 1) continue;
+			grunt.log.debug('Got match, options are { ' + matches[1] + ' }.');
+			var optionsObj = JSON.parse("{" + matches[1] + "}");
+			results.push({
+				start: matches.index,
+				end: matches.index + matches[0].length - 1,
+				options: optionsObj
+			});
+		}
+
+		return results;
+	}
+
+	var parsers = {
+		'html': parseHtml,
+		'scss': parseSass
+	};
+
+	var templates = {
+		'html':
+		{
+			'js': '<script src="{filePath}"></script>',
+			'css': '<link href="{filePath}" rel="stylesheet" type="text/css" />'
+		},
+		'scss':
+		{
+			'scss': '@import "{filePath}";'
+		}
+	};
+
+	var resolveFiles = function (basePath, includeOptions) {
+		if (includeOptions.basePath) {
+			basePath = includeOptions.basePath;
+		}
+		grunt.log.debug('Resolving files on base path "' + basePath + '"...');
+		grunt.log.debug('Include options: ' + util.inspect(includeOptions));
+
+		var files, sourcePath = '';
+		if (includeOptions.bower) {
+			grunt.log.debug('Resolving files from Bower component "' + includeOptions.bower + '"...');
+
+			sourcePath =  path.join('bower_components', includeOptions.bower);
+			basePath = path.join(basePath, sourcePath);
+			grunt.log.debug('Full path is "' + basePath + '".');
+
+			var sources = grunt.file.readJSON(path.join(basePath, 'bower.json')).sources;
+			files = sources[includeOptions.type];
+		} else {
+			grunt.log.debug('Resolving files from include property...');
+			files = includeOptions.files;
+		}
+
+		if (!files) {
+			grunt.log.debug('No files found.');
+			return [];
+		}
+
+		grunt.log.debug('Expanding files: ' + util.inspect(files));
+		var expandedFiles = grunt.file.expand({ cwd: basePath }, files);
+
+		var results = [];
+		for (var i = 0; i < expandedFiles.length; ++i) {
+			var file = path.join(sourcePath, expandedFiles[i]).replace(/\\/g, '/');
+			grunt.log.debug('Found file "' + file + '".');
+			results.push(file);
+		}
+
+		return results;
+	};
+
+	// Register the task.
+	grunt.registerMultiTask('includeSource', 'Include lists of files into your source files automatically.', function() {
+		grunt.log.debug('Starting task "includeSource"...');
+
+		var options = this.options({
+			basePath: '',
+			vars: {}
+		});
+
+		grunt.log.debug('Base path is "' + options.basePath + '".');
+
+		// Iterate over all specified file groups.
+		this.files.forEach(function(file) {
+			grunt.log.debug('Handling output file "' + file.dest + "'...");
+
+			// Concatenate the source files.
+			var contents = file.src.filter(function(filePath) {
+					grunt.log.debug('Using input file "' + filePath + '".');
+					// Remove nonexistent files.
+					if (!grunt.file.exists(filePath)) {
+						grunt.log.warn('Source file "' + filePath + '" not found.');
+						return false;
+					} else {
+						return true;
+					}
+				}).map(	function(filePath) {
+					// Read and return the file's source.
+					return grunt.file.read(filePath);
+				}).join('\n');
+
+			// Parse the contents, using a parser based on the target file.
+			var fileType = path.extname(file.dest).substr(1);
+			grunt.log.debug('File type is "' + fileType + '"');
+
+			var parserFn = parsers[fileType];
+
+			if (!parserFn) {
+				grunt.log.error('No parser found found file type "' + fileType + '".');
+				return false;
+			}
+
+			// Get the available templates.
+			var typeTemplates = templates[fileType];
+
+			// Get the includes and rewrite the contents.
+			var includes = parserFn(contents);
+			var currentOffset = 0;
+
+			grunt.log.debug('Found ' + includes.length + ' include statement(s).');
+
+			includes.forEach(function(include) {
+				var files = resolveFiles(options.basePath, include.options);
+				var	includeFragments = [];
+				files.forEach(function(file) {
+					grunt.log.debug('Including file "' + file + '".');
+					includeFragments.push(typeTemplates[include.options.type].replace('{filePath}', file));
+				});
+
+				var includeFragment = includeFragments.join(os.EOL);
+
+				contents =
+					contents.substr(0, include.start + currentOffset) +
+					includeFragment +
+					contents.substr(include.end + 1 + currentOffset);
+
+				var originalLength = include.end - include.start + 1;
+				currentOffset += includeFragment.length - originalLength;
+			});
+
+			grunt.log.debug('Writing output...');
+			grunt.file.write(file.dest, contents);
+			grunt.log.writeln('File "' + file.dest + '" created.');
+		});
+	});
+};
