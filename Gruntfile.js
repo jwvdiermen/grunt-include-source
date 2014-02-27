f93cc9f61efc344852eee3462f811f6be301ee84
paulbdavis <paul@dangersalad.com>
2014-02-27 13:14:10 -0500

    use global config to parse template variables in basePath strings

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
     },
 
     // Configuration to be run (and then tested).
-    include_source: {
+    includeSource: {
       default_options: {
         options: {
         },
@@ -65,7 +65,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'include_source', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'includeSource', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
