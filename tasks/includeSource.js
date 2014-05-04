f7bf073a8dde46add351d21a83d9f7fa68b5ed82
Jan Willem van Diermen <jwvdiermen@gmail.com>
2014-05-04 14:17:40 +0200

    Update regex for JADE to support additional comment type

--- tasks/includeSource.js
+++ tasks/includeSource.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
 	var parsers = {
 		'html': parseSource('HTML', /<!---?\s*include:\s+(.*)\s*-?--\s*>/gi),
 		'haml': parseSource('HAML', /-#\s+include:\s+(.*)/gi),
-		'jade': parseSource('JADE', /\/\/\s+include:\s+(.*)/gi),
+		'jade': parseSource('JADE', /\/\/-?\s+include:\s+(.*)/gi),
 		'scss': parseSource('SASS', /\/\/\s+include:\s+(.*)/gi),
 		'less': parseSource('LESS', /\/\/\s+include:\s+(.*)/gi)
 	};
